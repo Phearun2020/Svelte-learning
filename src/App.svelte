a39f745010a29487005c6f69510610a21aeef0ed
Phearun2020 <phearun.ph77@gmail.com>
2022-02-28 13:50:14 +0700

    inline event-delete

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,17 @@
 <script>
 	let students = [
-		// {name: 'Dara', beltColour: 'yellow', age: '21', id: 1},
-		// {name: 'Thara', beltColour: 'blue', age: '22', id: 2},
-		// {name: 'Both', beltColour: 'black', age: '23', id: 3},
-		// {name: 'Ramos', beltColour: 'green', age: '21', id: 4},
+		{name: 'Dara', beltColour: 'yellow', age: '21', id: 1},
+		{name: 'Thara', beltColour: 'blue', age: '22', id: 2},
+		{name: 'Both', beltColour: 'black', age: '23', id: 3},
+		{name: 'Ramos', beltColour: 'green', age: '21', id: 4},
 	];
+
+	const handleClick = (id) =>{
+		// delete the person from students
+		// console.log(id);
+		students = students.filter((person) => person.id != id);
+
+	}
 </script>
 
 <main>
@@ -12,6 +19,7 @@
 		<div>
 			<h4>id {person.id} {person.name}</h4>
 			<p>{person.age} year old, {person.beltColour} belt.</p>
+			<button on:click={() => {handleClick(person.id)}}>Delete</button>
 		</div>
 		{:else}
 			<p>There are no students to show ...</p>
