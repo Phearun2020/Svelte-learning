f8f6ca0a2dde336e3400b5f00bfcf9b4771e7c63
Phearun2020 <phearun.ph77@gmail.com>
2022-02-28 12:32:18 +0700

    test-button and text

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+	
+	let hello = 'Phearun';
+	let beltColour = 'blue';
+	const handleClick =()=>{
+		beltColour = 'okay';
+	};
+	const handleInput = (e) =>{
+		beltColour = e.target.value;
+	}
+</script>
+
+<main>
+	<h1>Hello {hello}!</h1>
+	<p>{beltColour}!</p>
+	<button on:click={handleClick}>click here</button>
+	<input type="text" on:input={handleInput}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
