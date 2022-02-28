f86b66ce3213c8e10c49831bdb35168c424e2148
Phearun2020 <phearun.ph77@gmail.com>
2022-02-28 14:13:00 +0700

    conditionals-if else

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script>
 	let students = [
-		{name: 'Dara', beltColour: 'yellow', age: '21', id: 1},
-		{name: 'Thara', beltColour: 'blue', age: '22', id: 2},
-		{name: 'Both', beltColour: 'black', age: '23', id: 3},
-		{name: 'Ramos', beltColour: 'green', age: '21', id: 4},
+		{name: 'Dara', beltColour: 'yellow', age: '21', score: 89, id: 1},
+		{name: 'Thara', beltColour: 'blue', age: '22', score: 35, id: 2},
+		{name: 'Both', beltColour: 'black', age: '23', score: 50, id: 3},
+		{name: 'Ramos', beltColour: 'green', age: '21', score: 45, id: 4},
 	];
 
 	const handleClick = (id) =>{
@@ -12,13 +12,23 @@
 		students = students.filter((person) => person.id != id);
 
 	}
+	let num = 50;
 </script>
 
 <main>
+	
 	{#each students as person (person.id)}
+		
 		<div>
 			<h4>id {person.id} {person.name}</h4>
-			<p>{person.age} year old, {person.beltColour} belt.</p>
+			<p>{person.age} year old, your score totaly {person.score}, {person.beltColour} belt.</p>
+			{#if num < person.score}
+				<p>Congraulation</p>
+				{:else if person.score == num}
+				 	<p>good luck</p>
+				{:else}
+					<p>you are lose.</p>
+			{/if}
 			<button on:click={() => {handleClick(person.id)}}>Delete</button>
 		</div>
 		{:else}
